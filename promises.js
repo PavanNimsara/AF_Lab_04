da286a677bac34edaf86cb11d9e7a1064db61195
PavanNimsara <pawanimsara12@gmail.com>
2026-02-21 16:30:52 +0530

    Add Node.js Promises feature using resolve and reject

---
+++ promises.js
@@ -0,0 +1,15 @@
+const condition = true; // Change to false to test reject
+
+const myPromise = new Promise((resolve, reject) => {
+    if (condition) {
+        resolve('Success!');
+    } else {
+        reject('Failure!');
+    }
+});
+
+myPromise.then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+});
